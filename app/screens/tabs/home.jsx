d7bcc6fbb15ba9ec95cb8a704cc5dec944d0476e
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-03-17 14:33:33 +0100

    update

--- app/screens/tabs/home.jsx
+++ app/screens/tabs/home.jsx
@@ -1,11 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hi trips overview</Text>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      <Text>My Trips</Text>
+      <Image style={styles.thumbnail} source={require('../../../assets/images/barcelona.png')} />
+      <Image style={styles.thumbnail} source={require('../../../assets/images/newyork.png')} />
+      <Image style={styles.thumbnail} source={require('../../../assets/images/barcelona.png')} />
+      <Image style={styles.thumbnail} source={require('../../../assets/images/barcelona.png')} />
+      <Image style={styles.thumbnail} source={require('../../../assets/images/barcelona.png')} />
+      </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  thumbnail: {
+    width: 410,
+    height: 200,
+    borderRadius: 10,
+    resizeMode: 'cover'
+  }
+  
+})
