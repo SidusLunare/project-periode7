b370365885bad8308126eab15166e37e5ef9fafe
pascalvdv <stefanpftf@gmail.com>
2025-03-26 10:42:23 +0100

    gfrwg

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,12 @@
 // utils/config.js
 
-const ENV = "development"; // Change to "production" when deploying
+const ENV = "home"; // Change to "production" when deploying
 
 const CONFIG = {
     development: {
         SERVER_URL: "http://192.168.1.92:3000",
     },
-    production: {
+    home: {
         SERVER_URL: "http://192.168.1.92:3000",
     },
 };
